9598c3c9e97d66460511a4ba1a0c69c54e49c451
sarpound <soravit.var@hotmail.com>
2024-02-28 22:22:04 +1100

    add mock app-header and app-input componen into fxforward

--- src/app/app-fxforward/app-fxforward.component.ts
+++ src/app/app-fxforward/app-fxforward.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-fxforward',
-  standalone: true,
-  imports: [ CommonModule, MatExpansionModule ],
   templateUrl: './app-fxforward.component.html',
-  styleUrl: './app-fxforward.component.less'
+  styleUrl: './app-fxforward.component.less',
+  standalone: true,
+  imports: [ CommonModule, MatExpansionModule, MatDatepickerModule, MatNativeDateModule, MatInputModule, MatIconModule ],
 })
 export class AppFxforwardComponent {
 
